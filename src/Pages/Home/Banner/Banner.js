132fc29fdc585b633e6d7a177aa6e809f2ae3ee5
imtiaz remon <imtiazremon10375@gmail.com>
2022-04-29 17:12:03 +0600

    banner add

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Carousel } from 'react-bootstrap';
+import pic1 from '../../../Image/banner/pic1.jpg'
+import pic2 from '../../../Image/banner/pic2.jpg'
+import pic3 from '../../../Image/banner/pic3.webp'
+
+const Banner = () => {
+    return (
+        <div>
+            <h1 className='text-center my-3'>Anik Gym warehouse management website</h1>
+
+            <Carousel>
+                <Carousel.Item>
+                    <img style={{ height: "80vh" }}
+                        className="d-block w-100"
+                        src={pic1}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>First slide label</h3>
+                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img style={{ height: "80vh" }}
+                        className="d-block w-100"
+                        src={pic2}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Second slide label</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img style={{ height: "80vh" }}
+                        className="d-block w-100"
+                        src={pic3}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>Third slide label</h3>
+                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+
+        </div>
+    );
+};
+
+export default Banner;
